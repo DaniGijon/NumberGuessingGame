8b63e682604eeb208203fa974b8c79206ee6d273
DaniGijon <dani91gr@hotmail.com>
2025-10-16 09:02:18 +0200

    first commit

---
+++ src/GameCLI/NumberGuessingGameCLI.java
@@ -0,0 +1,10 @@
+package GameCLI;
+
+public class NumberGuessingGameCLI {
+	
+	public static void main (String args []) {
+		System.out.println("Welcome to the Number Guessing Game!\n"
+				+ "I'm thinking of a number between 1 and 100.\n"
+				+ "You have 5 chances to guess the correct number.");
+	}
+}
